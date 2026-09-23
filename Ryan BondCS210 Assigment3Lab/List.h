7fc792aaece863caf77999a324b44133efc285ec
Ryan Bond <ryanbondaudio@gmail.com>
2026-09-22 19:32:29 -0700

    Created the List.h file and implemented the necessary virtual functions of the class.

---
+++ Ryan BondCS210 Assigment3Lab/List.h
@@ -0,0 +1,25 @@
+//
+// Created by Ryan Bond on 9/22/26.
+//
+
+#pragma once
+#include <memory>
+
+template <typename T>
+class List {
+public:
+    virtual ~List() = default;
+    
+    virtual void addFront(T* value) = 0;
+    virtual void deleteFront() = 0;
+    virtual bool search(T* value) const = 0;
+    virtual void print() const = 0;
+};
+
+#include "ArrayList.h"
+#include "LinkedList.h"
+
+template <typename T>
+std::unique_ptr<List<T>> makeList () {
+    return std::make_unique<List<T>>();
+}
